57170f46d1c7849e131d333a05dff1a60c3298b8
Alexandr Bolotin <alexbolotin89@gmail.com>
2022-07-07 15:46:25 +0300

    1

--- django_test/src/proj/settings.py
+++ django_test/src/proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xas027x+@lx1cb9jnqm@yjn@zo@ps#s^!ay%ap_+g+71gv(5y8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Bolotin.pythonanywhere.com']
 
 
 # Application definition
