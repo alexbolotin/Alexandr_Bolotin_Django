f014b7336650bc7a3ac81e9fb689718fd4508314
Alexandr Bolotin <alexbolotin89@gmail.com>
2022-07-07 16:22:06 +0300

    1

--- django_test/src/hello_world/views.py
+++ django_test/src/hello_world/views.py
@@ -11,5 +11,5 @@ def hello_world_view(request):
     # name = NAMES[randint(0,2)]
     clock = datetime.datetime.now()
     time_str = clock.strftime("%H:%M:%S")
-    return HttpResponse(f"<h1>Hello {request.user}</h1>\n<h1>Hello {time_str}</h1>")
+    return HttpResponse(f"<h1>Hello {request.user}</h1>\n<h1>Time of request - {time_str}</h1>")
 
