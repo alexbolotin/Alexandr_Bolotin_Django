bbd667b8546c0631c3f545028c3f2318e3c81688
Alexandr Bolotin <alexbolotin89@gmail.com>
2022-07-07 16:21:09 +0300

    1

--- django_test/src/hello_world/views.py
+++ django_test/src/hello_world/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import HttpResponse, render
 from random import randint
+import datetime
+import time
+
 # Create your views here.
 NAMES = ('Ivan', 'Kolia', 'Vasia')
+
+
 def hello_world_view(request):
     # name = NAMES[randint(0,2)]
-    return HttpResponse(f"<h1>Hello {request.user}</h1>")
+    clock = datetime.datetime.now()
+    time_str = clock.strftime("%H:%M:%S")
+    return HttpResponse(f"<h1>Hello {request.user}</h1>\n<h1>Hello {time_str}</h1>")
+
