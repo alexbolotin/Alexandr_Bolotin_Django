b79bc7a7112da61b6ebfb3458ef98411a05eff78
Alexandr Bolotin <alexbolotin89@gmail.com>
2022-07-07 16:17:18 +0300

    1

--- django_test/src/hello_world/views.py
+++ django_test/src/hello_world/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import HttpResponse, render
 from random import randint
 # Create your views here.
 NAMES = ('Ivan', 'Kolia', 'Vasia')
-def hello_world_view(words):
-    name = NAMES[randint(0,2)]
-    return HttpResponse(f"<h1>Hello {words.user}</h1>")
+def hello_world_view(request):
+    # name = NAMES[randint(0,2)]
+    return HttpResponse(f"<h1>Hello {request.user}</h1>")
