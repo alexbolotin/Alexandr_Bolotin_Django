267d0747150a3c8b44c29076804a009ee55867f9
Alexandr Bolotin <alexbolotin89@gmail.com>
2022-07-27 13:59:53 +0300

    1

--- django_test/src/proj/settings.py
+++ django_test/src/proj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = local_vars.DJANGO_KEY
 DEBUG = True
 
 ALLOWED_HOSTS = ['bolotin.pythonanywhere.com']
-ALLOWED_HOSTS = ['127.0.0.1']
+# ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
